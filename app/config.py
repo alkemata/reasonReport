8219530377079402e7a8a34b1a4dc15923cf6493
alkemata <42153661+alkemata@users.noreply.github.com>
2024-08-16 08:07:36 +0200

    test

--- app/config.py
+++ app/config.py
@@ -1,6 +1,9 @@
 import os
+SECRET_KEY =  'super-secret-key'
+JWT_SECRET_KEY = 'super-secret-jwt-key'
+MONGO_URI = 'mongodb://localhost:27017/notebooks_db'
 
-class Config:
+""" class Config:
     SECRET_KEY =  'super-secret-key'
     JWT_SECRET_KEY = 'super-secret-jwt-key'
-    MONGO_URI = 'mongodb://localhost:27017/notebooks_db'
+    MONGO_URI = 'mongodb://localhost:27017/notebooks_db' """
