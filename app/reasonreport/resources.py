d79d619c0f81311577934a6139e38efce487d9dd
alkemata <42153661+alkemata@users.noreply.github.com>
2024-10-26 14:47:42 +0200

    correct

--- app/reasonreport/resources.py
+++ app/reasonreport/resources.py
@@ -104,7 +104,7 @@ class NotebookSave(Resource):
         if notebook['author'] != request.user['id']:
             return {'message': 'Unauthorized access to this notebook'}, 403
         
-        save_notebook(notebook_id, args['notebook'])
+        save_notebook(notebook_id, json.loads(args['notebook']))
         return {'message': 'OK'}, 200
 
 class NotebookQuery(Resource):
