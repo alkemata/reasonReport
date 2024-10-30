4c6edf5d2cb0f29c5624ab088c6f36e3ff577fb1
alkemata <42153661+alkemata@users.noreply.github.com>
2024-10-30 21:49:25 +0100

    debug

--- app/reasonreport/resources.py
+++ app/reasonreport/resources.py
@@ -91,6 +91,7 @@ class NotebookSave(Resource):
         parser = reqparse.RequestParser()
         parser.add_argument('notebook', required=True, help="Notebook JSON is required")
         args = parser.parse_args()
+        print(args['notebook'])
         current_app.logger.info(args['notebook'])
         
         notebook = get_notebook(notebook_id) # for checking authour
